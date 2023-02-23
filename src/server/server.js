f0f43cf450ae316e50ef0c35e707f5e049b0c2b3
Harrison Caughey <harrison.caughey@adscale.co.nz>
2023-02-24 11:15:18 +1300

    Updated dev server

--- src/server/server.js
+++ src/server/server.js
@@ -32,10 +32,14 @@ app.use(function (req, res, next) {
 });
 
 app.get('/api/games', (req, res) => {
-    db.select('Game.id', 'Game.date_played', 'Game.player_one', 'Game.player_two', 'Game.score', 'Game.player_one_win', 'Player.name as p1_name', 'Player2.name as p2_name')
-            .from('Game')
-            .leftJoin('Player', 'Game.player_one', 'Player.id')
-            .leftJoin('Player as Player2', 'Game.player_two', 'Player2.id')
+    let offset = (req.query.page - 1) * 10;
+    let order = req.query.order;
+    let direction = req.query.direction
+    db.select('*')
+            .from('Games')
+            .orderBy(order, direction)
+            .offset(offset)
+            .limit(10)
             .then((data) => {
                 res.json(data);
             })
@@ -46,14 +50,19 @@ app.get('/api/games', (req, res) => {
 
 app.post('/api/games', (req, res) => {
     let game = req.body.game
-    db('Game').insert(
-            {player_one: game.player_one,
-                player_two: game.player_two,
-                player_one_win: game.player_one_win,
-                score: game.score,
-                date_played: game.date_played,
-            })
+    db('Games').insert(
+            {red_team: game.red_team,
+                blue_team: game.blue_team,
+                red_score: game.red_score,
+                blue_score: game.blue_score,
+                red_possession: game.red_possession,
+                blue_possession: game.blue_possession,
+                date: game.date,
+                game_time: game.game_time,
+                binary_id: game.binary_id,
+            }, 'id')
             .then((data) => {
+                console.log(data)
                 res.json(data);
             })
             .catch((err) => {
@@ -63,7 +72,7 @@ app.post('/api/games', (req, res) => {
 
 app.delete('/api/games', (req, res) => {
     let id = req.body.id
-    db('Game').where({id: id}).del()
+    db('Games').where({id: id}).del()
             .then((data) => {
                 res.json(data)
             }).catch((err) => {
@@ -74,7 +83,7 @@ app.delete('/api/games', (req, res) => {
 
 app.get('/api/players', (req, res) => {
     db.select('*')
-            .from('Player')
+            .from('Players')
             .then((data) => {
                 res.json(data);
             })
@@ -86,11 +95,149 @@ app.get('/api/players', (req, res) => {
 app.put('/api/players', (req, res) => {
     let player = req.body.player;
     let id = req.body.id;
-    db('Player').where({id: id}).update(player).then((data) => {
-        res.json(data);
-    }).catch((err) => {
+    db('Players').where({id: id})
+        .update({
+            name: player.name,
+            games_played: player.games_played,
+            games_won: player.games_won,
+            elo: player.elo,
+            mvps: player.mvps,
+            goals: player.goals,
+            assists: player.assists,
+            kicks: player.kicks,
+            passes: player.passes,
+            shots_on_goal: player.shots_on_goal,
+            own_goals: player.own_goals
+        })
+        .then((data) => {
+            res.json(data);
+        }).catch((err) => {
+            console.log(err)
+        })
+})
+
+app.post('/api/players', (req, res) => {
+    let player = req.body.player;
+    db('Players').insert(player, 'id')
+        .then((data) => {
+            res.json(data);
+        }).catch((err) => {
+            console.log(err)
+        })
+})
+
+app.get('/api/playerGameStats', (req, res) => {
+    let playerId = req.query.player_id;
+    let gameId = req.query.game_id
+    db.select('*')
+        .from('PlayerGameStats')
+        .where({
+            game_id: gameId,
+            player_id: playerId
+        })
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
+
+app.post('/api/playerGameStats', (req, res) => {
+    let player = req.body.playerGameStats
+    db('PlayerGameStats').insert(
+        {
+            game_id: player.game_id,
+            player_id: player.player_id,
+            goals: player.goals,
+            assists: player.assists,
+            kicks: player.kicks,
+            passes: player.passes,
+            shots_on_goal: player.shots_on_goal,
+            own_goals: player.own_goals,
+            won: player.won
+        })
+        .then((data) => {
+            console.log(data)
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
+
+app.delete('/api/playerGameStats', (req, res) => {
+    let id = req.body.id
+    db('PlayerGameStats').where({id: id}).del()
+        .then((data) => {
+            res.json(data)
+        }).catch((err) => {
         console.log(err)
+        res.status(500).message(err)
     })
+})
+
+app.get('/api/binary', (req, res) => {
+    db.select('*')
+        .from('Binaries')
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
+
+app.post('/api/binary', (req, res) => {
+    let file = req.body.file
+    db('Binaries').insert(
+        {
+            file: file
+        })
+        .then((data) => {
+            console.log(data)
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
+
+app.delete('/api/binary', (req, res) => {
+    let id = req.body.id
+    db('Binaries').where({id: id}).del()
+        .then((data) => {
+            res.json(data)
+        }).catch((err) => {
+        console.log(err)
+        res.status(500).message(err)
+    })
+})
+
+app.get('/api/pseudonyms', (req, res) => {
+    db.select('*')
+        .from('Pseudonyms')
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
+
+app.post('/api/pseudonyms', (req, res) => {
+    let pseudonyms = req.body.pseudonyms
+    db('Pseudonyms').insert(
+        {
+            pseudonyms: pseudonyms
+        })
+        .then((data) => {
+            console.log(data)
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 })
 
 app.listen(port, () => {
